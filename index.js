0bdebddd911c817b1309925fd50dbf26f083c9b6
AyaadChouhan <136963590+AyaadChouhan@users.noreply.github.com>
2023-12-22 15:42:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const questionEl = document.getElementById(".ques");
+const imgEl = document.querySelector(".bg-img");
 
-let num1 = Math.ceil(Random.Random)*10;
+const contEL = document.querySelector(".container");
+
+window.addEventListener("scroll", ()=>{
+    updateImg();
+})
+
+function updateImg(){
+   imgEl.style.opacity = 1 - window.pageYOffset / 800;
+   imgEl.style.backgroundSize = 140 - window.pageYOffset / 12 + "%";
+}
